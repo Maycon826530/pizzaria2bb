d28b9dc9e4f350a581db4e24f40a49415866935d
programadormovel <programadormovel@gmail.com>
2025-09-19 11:12:36 -0300

    pizzaria 2b oficial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Produtos from "./pages/produtos"
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Projeto Pizzaria</h1>
+      <Produtos />
+      
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+const Produtos = () => {
+    const [pizzas, setPizzas] = useState([])
+
+    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+    .then(response=>setPizzas(response.data.data))
+
+    // Mapeamento das pizzas da lista (iteração)
+    const listaPizzas = pizzas.map(pizza =>
+                    <li key={pizza.id}>{pizza.nome}</li>);
+    return(
+    <>
+        <h3>Listagem de Produtos</h3>
+        <ul>
+            {listaPizzas}
+        </ul>
+    </>
+    )
+}
+export default Produtos
